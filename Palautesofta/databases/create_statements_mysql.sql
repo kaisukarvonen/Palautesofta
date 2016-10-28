0f3c0cbabee855c1cc39d3fa38dcfaf9fcc11487
taruhokkanen <tarhokkanen@gmail.com>
2016-10-28 13:14:04 +0300

    Update create_statements_mysql.sql

--- Palautesofta/databases/create_statements_mysql.sql
+++ Palautesofta/databases/create_statements_mysql.sql
@@ -2,11 +2,11 @@ CREATE TABLE kysymys (
 kysymys_id
 ,nimi
 PRIMARY KEY(kysymys_id)
-)
+);
 
 CREATE TABLE vastaus (
 vastaus_id
 ,nimi
 PRIMARY KEY(vastaus_id)
 REFERENCES kysymys (kysymys_id)
-)
+);
