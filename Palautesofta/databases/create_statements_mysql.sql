f39a7daaab1c39c0eb20124861ae795fdb2ca455
taruhokkanen <tarhokkanen@gmail.com>
2016-10-28 13:35:48 +0300

    sql-lauseiden muokkaus

--- Palautesofta/databases/create_statements_mysql.sql
+++ Palautesofta/databases/create_statements_mysql.sql
@@ -7,7 +7,7 @@ PRIMARY KEY(kysymys_id)
 CREATE TABLE vastaus (
 vastaus_id integer NOT NULL auto_increment
 ,nimi varchar(255) NOT NULL
-,kysymys_id int DEFAULT NULL
+,kysymys_id int NOT NULL
 PRIMARY KEY(vastaus_id)
 FOREIGN KEY (kysymys_id) REFERENCES kysymys (kysymys_id)
 ); 
