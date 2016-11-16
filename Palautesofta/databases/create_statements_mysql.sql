f73f8d1ed07055d99b74ff206967794a5f07bdd3
taruhokkanen <tarhokkanen@gmail.com>
2016-11-16 12:35:15 +0200

    luotu kysymys_tyyppi ja muokattu kysymys-taulua

--- Palautesofta/databases/create_statements_mysql.sql
+++ Palautesofta/databases/create_statements_mysql.sql
@@ -9,8 +9,16 @@ CREATE TABLE kysymys (
 kysymys_id integer NOT NULL auto_increment
 ,kysely_id integer NOT NULL
 ,kysymys_nimi varchar(255)
+,tyyppi_id integer NOT NULL
 ,PRIMARY KEY(kysymys_id)
 ,FOREIGN KEY (kysely_id) REFERENCES kysely (kysely_id)
+,FOREIGN KEY (tyyppi_id) REFERENCES kysymys_tyyppi (tyyppi_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE kysymys_tyyppi (
+tyyppi_id integer NOT NULL
+,tyyppi_nimi varchar(255) NOT NULL
+,PRIMARY KEY(tyyppi_id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE vastaus (
