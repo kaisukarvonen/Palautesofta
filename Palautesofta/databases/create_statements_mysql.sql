205e535f87830eaa51286694a2c46200933fbdf3
taruhokkanen <tarhokkanen@gmail.com>
2016-11-18 12:30:23 +0200

    lisätty admin, authority ja admin_authority
    muutettu taulujen rakennetta

--- Palautesofta/databases/create_statements_mysql.sql
+++ Palautesofta/databases/create_statements_mysql.sql
@@ -1,8 +1,38 @@
+CREATE TABLE authority (
+authority_id integer NOT NULL auto_increment
+,role varchar(255) NOT NULL UNIQUE
+,PRIMARY KEY(authority_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ 
+CREATE TABLE admin (
+admin_id integer NOT NULL auto_increment
+,username varchar(255) NOT NULL UNIQUE
+,password_encrypted varchar(255) NOT NULL
+,enabled tinyint NOT NULL
+,PRIMARY KEY(admin_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE admin_authority (
+admin_authority_id integer NOT NULL auto_increment
+,admin_id integer NOT NULL,
+,authority_id integer NOT NULL,
+,PRIMARY KEY(admin_authority_id)
+,FOREIGN KEY (admin_id) REFERENCES admin(admin_id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+,FOREIGN KEY (authority_id) REFERENCES authority(authority_id) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE kysely (
 kysely_id integer NOT NULL auto_increment
 ,kysely_nimi varchar(255) NOT NULL
 ,kysely_kuvaus varchar(255)
 ,PRIMARY KEY(kysely_id)
+,FOREIGN KEY(admin_id) REFERENCES admin (admin_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE kysymys_tyyppi (
+tyyppi_id integer NOT NULL
+,tyyppi_nimi varchar(255) NOT NULL
+,PRIMARY KEY(tyyppi_id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE kysymys (
@@ -13,12 +43,6 @@ kysymys_id integer NOT NULL auto_increment
 ,PRIMARY KEY(kysymys_id)
 ,FOREIGN KEY (kysely_id) REFERENCES kysely (kysely_id)
 ,FOREIGN KEY (tyyppi_id) REFERENCES kysymys_tyyppi (tyyppi_id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE kysymys_tyyppi (
-tyyppi_id integer NOT NULL
-,tyyppi_nimi varchar(255) NOT NULL
-,PRIMARY KEY(tyyppi_id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE vastaus (
